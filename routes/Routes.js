8809e11f8aee0adfb9b5249258420957d052b7f7
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-10-25 21:05:43 +0530

    .

--- routes/Routes.js
+++ routes/Routes.js
@@ -4,7 +4,7 @@ import { addUser, getUser } from "../controller/userController.js";
 import { newConversation, getConversation } from "../controller/conversationController.js";
 import { msgNew, getMsg } from "../controller/msgController.js";
 import { uploadFile, getImage } from "../controller/image-controller.js";
-import upload from '../utils/upload.js';
+// import upload from '../utils/upload.js';
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
@@ -18,7 +18,7 @@ router.post("/conversation/add", newConversation);
 router.post("/conversation/get", getConversation);
 router.post("/message/add", msgNew);
 router.get("/message/get/:id", getMsg);
-router.post('/file/upload', upload.single('file'), uploadFile);
-router.get('/file/:filename', getImage);
+// router.post('/file/upload', upload.single('file'), uploadFile);
+// router.get('/file/:filename', getImage);
 
 export default router;
