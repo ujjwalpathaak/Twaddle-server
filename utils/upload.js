cbc87d1c44ae2aa6cab2fdf0ee94a1ca1565e377
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-10-25 20:55:08 +0530

    .

--- utils/upload.js
+++ utils/upload.js
@@ -1,24 +1,24 @@
-import multer from 'multer';
-import { GridFsStorage } from 'multer-gridfs-storage';
+import multer from "multer";
+import { GridFsStorage } from "multer-gridfs-storage";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const storage = new GridFsStorage({
-    url: process.env.DATABASE_URL,
-    options: { useNewUrlParser: true },
-    file: (request, file) => {
-        const match = ["image/png", "image/jpg"];
+  url: process.env.DATABASE_URL,
+  options: { useNewUrlParser: true },
+  file: (request, file) => {
+    const match = ["image/png", "image/jpg"];
 
-        if(match.indexOf(file.memeType) === -1) 
-            return`${Date.now()}-blog-${file.originalname}`;
+    if (match.indexOf(file.memeType) === -1)
+      return `${Date.now()}-blog-${file.originalname}`;
 
-        return {
-            bucketName: "photos",
-            filename: `${Date.now()}-blog-${file.originalname}`
-        }
-    }
+    return {
+      bucketName: "photos",
+      filename: `${Date.now()}-blog-${file.originalname}`,
+    };
+  },
 });
 
-export default multer({storage}); 
+export default multer({ storage });
