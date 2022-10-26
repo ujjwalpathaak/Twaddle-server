a60495813e45ce5ffce9ef1db79520ab8519319b
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-10-26 18:38:54 +0530

    .

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,38 +1,36 @@
-import grid from "gridfs-stream";
-import mongoose from "mongoose";
+import grid from 'gridfs-stream';
+import mongoose from 'mongoose';
 
-const url = "https://twaddle-server.herokuapp.com";
+const url = 'https://twaddle-server.herokuapp.com'
 
 let gfs, gridfsBucket;
 const conn = mongoose.connection;
-conn.once("open", () => {
-  gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
-    bucketName: "fs",
-  });
-  gfs = grid(conn.db, mongoose.mongo);
-  gfs.collection("fs");
+conn.once('open', () => {
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: 'fs'
+    });
+    gfs = grid(conn.db, mongoose.mongo);
+    gfs.collection('fs');
 });
 
-export const uploadFile = (request, response) => {
-  if (!request.file) return response.status(404).json("File not found");
 
-  const imageUrl = `${url}/file/${request.file.filename}`;
-
-  var delayInMilliseconds = 5000; //1 second
-
-  setTimeout(function () {
-    response.status(200).json(imageUrl);
-  }, delayInMilliseconds);
-};
+export const uploadFile =  (request, response) => {
+    if(!request.file) 
+        return response.status(404).json("File not found");
+    
+    const imageUrl = `${url}/file/${request.file.filename}`;
+    
+     response.status(200).json(imageUrl);    
+}
 
 export const getImage = async (request, response) => {
-  try {
-    const file = await gfs.files.findOne({ filename: request.params.filename });
-    // const readStream = gfs.createReadStream(file.filename);
-    // readStream.pipe(response);
-    const readStream = gridfsBucket.openDownloadStream(file._id);
-    readStream.pipe(response);
-  } catch (error) {
-    response.status(500).json({ msg: error.message });
-  }
-};
+    try {   
+        const file = await gfs.files.findOne({ filename: request.params.filename });
+        // const readStream = gfs.createReadStream(file.filename);
+        // readStream.pipe(response);
+        const readStream = gridfsBucket.openDownloadStream(file._id);
+        readStream.pipe(response);
+    } catch (error) {
+        response.status(500).json({ msg: error.message });
+    }
+}
