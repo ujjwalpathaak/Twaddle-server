a814e4c1aeb24b1aac2786fd9af0f949b74da065
Ujjwal Pathak <pathak2002ujjwal@gmail.com>
2023-05-09 19:02:45 +0530

    .

--- utils/upload.js
+++ utils/upload.js
@@ -5,19 +5,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const storage = new GridFsStorage({
-  url: process.env.DATABASE_URL,
-  options: { useNewUrlParser: true },
-  file: (request, file) => {
-    const match = ["image/png", "image/jpg", "image/jpeg", "image/gif"];
-    if (match.indexOf(file.mimetype) === -1) {
-      return `${Date.now()}-file-${file.originalname}`;
-    }
-    return {
-      bucketName: "photos",
-      filename: `${Date.now()}-file-${file.originalname}`,
-    };
-  },
-});
+// const storage = new GridFsStorage({
+//   url: process.env.DATABASE_URL,
+//   options: { useNewUrlParser: true },
+//   file: (request, file) => {
+//     const match = ["image/png", "image/jpg", "image/jpeg", "image/gif"];
+//     if (match.indexOf(file.mimetype) === -1) {
+//       return `${Date.now()}-file-${file.originalname}`;
+//     }
+//     return {
+//       bucketName: "photos",
+//       filename: `${Date.now()}-file-${file.originalname}`,
+//     };
+//   },
+// });
 
-export default multer({ storage });
+export default multer();
